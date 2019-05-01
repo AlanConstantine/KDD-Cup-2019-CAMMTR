d7458ce83ff71c209232a0f27c09281c385ac401
AlanConstantine <rlalan@outlook.com>
2019-05-01 21:54:25 +0800

    Add pipline.py

---
+++ Code/Pipline/pipline.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# @Author: Alan Lau
+# @Date: 2019-05-01 21:41:47
+
+
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+import os
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from tqdm import tqdm
+import json
+from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from itertools import product
+import ast
+
+
+def load_data(mainpath):
+    train_queries = pd.read_csv(
+        mainpath + 'train_queries.csv', parse_dates=['req_time'])
+    train_plans = pd.read_csv(mainpath + 'train_plans.csv',
+                              parse_dates=['plan_time'])
+    train_clicks = pd.read_csv(mainpath + 'train_clicks.csv')
+    profiles = pd.read_csv(mainpath + 'profiles.csv')
+    test_queries = pd.read_csv(
+        mainpath + 'test_queries.csv', parse_dates=['req_time'])
+    test_plans = pd.read_csv(mainpath + 'test_plans.csv',
+                             parse_dates=['plan_time'])
+    return train_queries, train_plans, train_clicks, profiles, test_queries, test_plans
+
+
+def merge(train_queries, train_plans, train_clicks, profiles, test_queries, test_plans):
+    train = train_queries.merge(train_plans, 'left', ['sid'])
+    test = test_queries.merge(test_plans, 'left', ['sid'])
+    train = train.merge(train_clicks, 'left', ['sid'])
+    train['click_mode'] = train['click_mode'].fillna(0).astype(int)
+    data = pd.concat([train, test], ignore_index=True)
+    data = data.merge(profiles, 'left', ['pid'])
+    return train.test, data
+
+
+def feature_engineering(data):
+    time_feature = []
+    data['time_diff'] = data['plan_time'].astype(
+        int) - data['req_time'].astype(int)
+    time_feature.append('time_diff')
+
+    data['plans_json'] = data['plans'].fillna(
+        '[]').apply(lambda x: json.loads(x))
+    data_plans = gen_plan_feas(data)
+    plan_features = [col for col in data_plans.columns if col not in ['sid']]
+    data = data.merge(data_plans, on='sid', how='left')
+
+
+def gen_plan_feas(data):
+    n = data.shape[0]
+    mode_list_feas = np.zeros((n, 12))
+    max_dist, min_dist, mean_dist, std_dist = np.zeros(
+        (n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+    max_price, min_price, mean_price, std_price = np.zeros(
+        (n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+    max_eta, min_eta, mean_eta, std_eta = np.zeros(
+        (n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+    min_dist_mode, max_dist_mode, min_price_mode, max_price_mode, min_eta_mode, max_eta_mode, first_mode = \
+        np.zeros((n,)), np.zeros((n,)), np.zeros((n,)), np.zeros(
+            (n,)), np.zeros((n,)), np.zeros((n,)), np.zeros((n,))
+
+    mode_texts = []
+    for i, plan in tqdm(enumerate(data['plans_json'].values)):
+        if len(plan) == 0:
+            cur_plan_list = []
+        else:
+            cur_plan_list = plan
+        if len(cur_plan_list) == 0:
+            mode_list_feas[i, 0] = 1
+            first_mode[i] = 0
+            max_dist[i] = -1
+            min_dist[i] = -1
+            mean_dist[i] = -1
+            std_dist[i] = -1
+            max_price[i] = -1
+            min_price[i] = -1
+            mean_price[i] = -1
+            std_price[i] = -1
+            max_eta[i] = -1
+            min_eta[i] = -1
+            mean_eta[i] = -1
+            std_eta[i] = -1
+            min_dist_mode[i] = -1
+            max_dist_mode[i] = -1
+            min_price_mode[i] = -1
+            max_price_mode[i] = -1
+            min_eta_mode[i] = -1
+            max_eta_mode[i] = -1
+            mode_texts.append('word_null')
+        else:
+            distance_list = []
+            price_list = []
+            eta_list = []
+            mode_list = []
+            for tmp_dit in cur_plan_list:
+                distance_list.append(int(tmp_dit['distance']))
+                if tmp_dit['price'] == '':
+                    price_list.append(0)
+                else:
+                    price_list.append(int(tmp_dit['price']))
+                eta_list.append(int(tmp_dit['eta']))
+                mode_list.append(int(tmp_dit['transport_mode']))
+            mode_texts.append(
+                ' '.join(['word_{}'.format(mode) for mode in mode_list]))
+            distance_list = np.array(distance_list)
+            price_list = np.array(price_list)
+            eta_list = np.array(eta_list)
+            mode_list = np.array(mode_list, dtype='int')
+            mode_list_feas[i, mode_list] = 1
+            distance_sort_idx = np.argsort(distance_list)
+            price_sort_idx = np.argsort(price_list)
+            eta_sort_idx = np.argsort(eta_list)
+            max_dist[i] = distance_list[distance_sort_idx[-1]]
+            min_dist[i] = distance_list[distance_sort_idx[0]]
+            mean_dist[i] = np.mean(distance_list)
+            std_dist[i] = np.std(distance_list)
+            max_price[i] = price_list[price_sort_idx[-1]]
+            min_price[i] = price_list[price_sort_idx[0]]
+            mean_price[i] = np.mean(price_list)
+            std_price[i] = np.std(price_list)
+            max_eta[i] = eta_list[eta_sort_idx[-1]]
+            min_eta[i] = eta_list[eta_sort_idx[0]]
+            mean_eta[i] = np.mean(eta_list)
+            std_eta[i] = np.std(eta_list)
+            first_mode[i] = mode_list[0]
+            max_dist_mode[i] = mode_list[distance_sort_idx[-1]]
+            min_dist_mode[i] = mode_list[distance_sort_idx[0]]
+            max_price_mode[i] = mode_list[price_sort_idx[-1]]
+            min_price_mode[i] = mode_list[price_sort_idx[0]]
+            max_eta_mode[i] = mode_list[eta_sort_idx[-1]]
+            min_eta_mode[i] = mode_list[eta_sort_idx[0]]
+    feature_data = pd.DataFrame(mode_list_feas)
+    feature_data.columns = ['mode_feas_{}'.format(i) for i in range(12)]
+    feature_data['max_dist'] = max_dist
+    feature_data['min_dist'] = min_dist
+    feature_data['mean_dist'] = mean_dist
+    feature_data['std_dist'] = std_dist
+    feature_data['max_price'] = max_price
+    feature_data['min_price'] = min_price
+    feature_data['mean_price'] = mean_price
+    feature_data['std_price'] = std_price
+    feature_data['max_eta'] = max_eta
+    feature_data['min_eta'] = min_eta
+    feature_data['mean_eta'] = mean_eta
+    feature_data['std_eta'] = std_eta
+    feature_data['max_dist_mode'] = max_dist_mode
+    feature_data['min_dist_mode'] = min_dist_mode
+    feature_data['max_price_mode'] = max_price_mode
+    feature_data['min_price_mode'] = min_price_mode
+    feature_data['max_eta_mode'] = max_eta_mode
+    feature_data['min_eta_mode'] = min_eta_mode
+    feature_data['first_mode'] = first_mode
+    print('mode tfidf...')
+    tfidf_enc = TfidfVectorizer(ngram_range=(1, 2))
+    tfidf_vec = tfidf_enc.fit_transform(mode_texts)
+    svd_enc = TruncatedSVD(n_components=10, n_iter=20, random_state=2019)
+    mode_svd = svd_enc.fit_transform(tfidf_vec)
+    mode_svd = pd.DataFrame(mode_svd)
+    mode_svd.columns = ['svd_mode_{}'.format(i) for i in range(10)]
+    plan_fea = pd.concat([feature_data, mode_svd], axis=1)
+    plan_fea['sid'] = data['sid'].values
+
+    return plan_fea
+
+
+def f1_weighted(labels, preds):
+    preds = np.argmax(preds.reshape(12, -1), axis=0)
+    score = f1_score(y_true=labels, y_pred=preds, average='weighted')
+    return 'f1_weighted', score, True
+
+
+def main():
+    mainpath = '../../Data/'
+    train.test, data = merge(load_data(mainpath))
+
+
+if __name__ == "__main__":
+    main()
